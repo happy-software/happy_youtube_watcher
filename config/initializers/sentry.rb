9e24beac1596bfa74dfa5d67f2e56c89d00a57e5
Hebron George <hebrontgeorge@gmail.com>
2024-08-19 16:56:09 -0400

    Uninstall Sentry

--- config/initializers/sentry.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sentry.init do |config|
-  config.dsn = ENV['SENTRY_DSN']
-  config.breadcrumbs_logger = [:active_support_logger]
-
-  # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.5
-end
