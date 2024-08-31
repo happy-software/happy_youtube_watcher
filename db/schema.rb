3301096cedfe571e842622a4513b15dc18c12737
Hebron George <hebrontgeorge@gmail.com>
2024-08-30 23:19:21 -0400

    Revert "Merge pull request #89 from happy-software/rails_update"
    This reverts commit e684df38e099d1280d2918efe0fff639c29435f5, reversing
    changes made to cc57f41dc99893031a3259a113514dc2de753f38.

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_17_174440) do
-  create_schema "_heroku"
-  create_schema "heroku_ext"
+ActiveRecord::Schema.define(version: 2023_07_16_224501) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
-
-  create_table "playlist_items", id: false, force: :cascade do |t|
-    t.integer "id", null: false
-    t.integer "playlist_snapshot_id"
-  end
 
   create_table "playlist_settings", force: :cascade do |t|
     t.string "playlist_id"
     t.jsonb "settings"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "playlist_snapshots", force: :cascade do |t|
     t.string "playlist_id"
     t.jsonb "playlist_items"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["playlist_id"], name: "index_playlist_snapshots_on_playlist_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "channel_username"
+    t.string "channel_id"
+    t.string "youtube_id"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["youtube_id"], name: "index_playlists_on_youtube_id", unique: true
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "video_id"
+    t.string "title"
+    t.string "uploader"
+    t.datetime "video_uploaded_date"
+    t.jsonb "description"
+    t.string "playlist_id"
+    t.string "playlist_index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tracked_playlists", force: :cascade do |t|
     t.string "playlist_id"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "is_default"
     t.string "name"
     t.string "channel_id"
@@ -51,4 +65,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_17_174440) do
     t.index ["name"], name: "index_tracked_playlists_on_name"
     t.index ["playlist_id"], name: "index_tracked_playlists_on_playlist_id", unique: true
   end
+
 end
