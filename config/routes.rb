a811bbfededbe750b30e10f48ce9ef6138093dfa
Hebron George <hebrontgeorge@gmail.com>
2024-09-02 10:51:35 -0400

    Revert "Revert "Reinstall Sidekiq""

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
 
   post '/shuffle',           to: 'playlists#shuffle'
   get '/tracked-playlists',  to: 'tracked_playlists#index'
