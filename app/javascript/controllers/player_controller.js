66ff778a5497ec556c59dbbb05f435ffa2547902
Hebron George <hebrontgeorge@gmail.com>
2025-08-01 16:27:30 -0400

    WIP trying to make multiple videos show up in the UI

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -4,12 +4,11 @@ export default class extends Controller {
   connect() {
     console.log(`Connected to player_controller.js from ${this.element}`)
     const container = document.getElementById('container')
-    const url = 'https://www.youtube.com/watch?v=d46Azg3Pm4c'
-
-    renderReactPlayer(container, { url, playing: true })
-  }
-
-  pausePlayer () {
-    renderReactPlayer(container, {url, playing: false})
+    const urls = ["https://www.youtube.com/watch?v=5h_hnvfStsE", "https://www.youtube.com/watch?v=S8YsF-sBf0Q"]
+    renderReactPlayer(container, {
+      url: urls,
+      playing: true,
+      controls: true
+    })
   }
 }
