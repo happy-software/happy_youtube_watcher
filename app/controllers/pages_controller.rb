9f8b127d6b15907b5aee18999d4deacdd77a9a52
Hebron George <hebrontgeorge@gmail.com>
2025-12-25 09:56:59 -0500

    Try appending local IPs after config initialization in case cloudflare-rails is overwriting it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
-
+    # TODO: Remove this after confirming remote_ip works correctly in prod
+    Rails.logger.warn(
+      remote_addr: request.remote_addr,
+      remote_ip:   request.remote_ip,
+      cf_ip:       request.headers["HTTP_CF_CONNECTING_IP"]
+    )
   end
 end
