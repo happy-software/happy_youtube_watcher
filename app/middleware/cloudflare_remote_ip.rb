c29b6d43fd121bcfa7e0224bb88cd22350ec1ed3
Hebron George <hebrontgeorge@gmail.com>
2025-12-25 10:49:41 -0500

    Try clearing any memoized ips

--- app/middleware/cloudflare_remote_ip.rb
+++ app/middleware/cloudflare_remote_ip.rb
@@ -8,6 +8,10 @@ class CloudflareRemoteIp
 
     if cf_ip.present?
       env["HTTP_X_FORWARDED_FOR"] ||= cf_ip
+
+      # clear memoized IPs
+      env.delete("action_dispatch.remote_ip")
+      env.delete("action_dispatch.client_ip")
     end
 
     @app.call(env)
