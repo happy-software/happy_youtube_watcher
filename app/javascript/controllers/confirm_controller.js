d6f54d6eb9a4a25e79754da82cf8595af9e0dc68
Hebron George <hebrontgeorge@gmail.com>
2026-06-27 17:04:15 -0400

    More Ahoy events

--- app/javascript/controllers/confirm_controller.js
+++ app/javascript/controllers/confirm_controller.js
@@ -1,8 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static values = { playlistId: String }
+
   askRemoveFavoritePlaylist(event) {
-    if (!confirm("Are you sure you want to remove this playlist?")) {
+    if (confirm("Are you sure you want to remove this playlist?")) {
+      ahoy.track("remove_favorite_playlist_confirmed", { playlist_id: this.playlistIdValue });
+    } else {
+      ahoy.track("remove_favorite_playlist_cancelled", { playlist_id: this.playlistIdValue });
       event.preventDefault();
     }
   }
